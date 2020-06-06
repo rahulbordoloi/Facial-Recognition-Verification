3164c63fd953e442c195fd8a77498857652291eb
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-06-07 00:24:50 +0530

    DeepFace

---
+++ DeepFace/deepface.py
@@ -0,0 +1,14 @@
+try:
+    import deepface
+    print("Succesfull")
+except ValueError:
+    print("Not Installed")
+
+
+# import numpy as np
+
+# print(DeepFace.__version__)
+# print(np.__version__)
+
+#import tensorflow as tf
+#print(tf.__version__)
